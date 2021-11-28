b0a43e884208e4760541994bc51a6fa322ee3c5b
Jean Cordeiro <38762540+JeanCSS@users.noreply.github.com>
2021-11-28 02:44:00 -0300

    update-buscaEmProfundidade

--- include/BuscaEmProfundidade.h
+++ include/BuscaEmProfundidade.h
@@ -6,12 +6,10 @@ class BuscaEmProfundidade{
     public:
         list<int> fila;
         list <int> :: iterator primeiroLista, ultimoLista;
-        int FinalFila, inicioFila;
+        int time;
 
 
         BuscaEmProfundidade(TGrafo grafo, int VericeInicial){
-            FinalFila = 0;
-            inicioFila = 0;
 
             for (int i = 0; i < grafo.getN(); i++){ /// zerar os vetores antes pois após execulatar uma vez todos ficam com cor preta
                 grafo.vertices[i].setId(i);
@@ -19,52 +17,45 @@ class BuscaEmProfundidade{
                 grafo.vertices[i].setDistancia(-1);
                 grafo.vertices[i].setPredecessorId(-1);
             }
-
-            grafo.vertices[VericeInicial].setCor("Cinza");
-            grafo.vertices[VericeInicial].setDistancia(0);
-            grafo.vertices[VericeInicial].setPredecessorId(-1);
-
+            time = 0;
             fila.push_back(VericeInicial);
             primeiroLista = fila.begin();
+            ultimoLista = fila.end();
 
-            printFila(fila, grafo.getN(), grafo.vertices);
-
-            visitar(grafo, *primeiroLista);
-
+            visitar(grafo, VericeInicial);
         }
 
     void visitar(TGrafo grafo, int id){
-
+        time++;
         cout << "Visitou o "<< id << endl;
+        grafo.vertices[id].setCor("Cinza");
+        grafo.vertices[id].setDistancia(time);
+
+        printFila(fila, grafo.getN(), grafo.vertices);
+        //grafo.vertices[id].setPredecessorId(-1);
+
         for(int i = 0; i < grafo.getN(); i++){
              if((grafo.MAdj[id][i] == 1 || grafo.MAdj[i][id] == 1) && grafo.vertices[i].getCor() == "Branco"){
-                grafo.vertices[i].setCor("Cinza");
-                grafo.vertices[i].setDistancia(grafo.vertices[id].getDistancia() + 1);
-                grafo.vertices[i].setPredecessorId(grafo.vertices[id].getId());
-
-                fila.push_back(grafo.vertices[i].getId());
-
+                cout << "Descobriu o vertice " << i <<endl;
+                grafo.vertices[i].setPredecessorId(id);
+                fila.push_back(i);
                 primeiroLista = fila.begin();
                 ultimoLista = fila.end();
 
-                cout << "Descobriu o vertice " << i <<endl;
-                FinalFila++;
+                visitar(grafo, i);
              }
-
         }
+
         printf("\n");
         grafo.vertices[id].setCor("Preto");
 
+        cout << "Retirado o " << id << " da pilha" << endl;
         printFila(fila, grafo.getN(), grafo.vertices);
+        fila.pop_back();
 
-        cout << "Retirado o " << *primeiroLista << " da lista" << endl;
-        fila.pop_front();
+
         primeiroLista = fila.begin();
         ultimoLista = fila.end();
-
-        if(fila.size() > 0){
-            visitar(grafo, *primeiroLista);
-        }
     }
 
     void printFila(list<int> fila, int n, TVertice v[]){
@@ -84,15 +75,13 @@ class BuscaEmProfundidade{
 
 
             cout << "   Q[ ";
-            //for(int i : fila){/// percore a fila de vertices e imprime os elementos que estão nela
-            for(list<int>::iterator i = fila.begin(); i!=fila.end(); i++){
+            /// for(int i : fila){/// percore a fila de vertices e imprime os elementos que estão nela
+            for(list<int>::iterator i = fila.begin(); i != fila.end(); i++){
                 cout << " | " << v[*i].getId() << ": " << v[*i].getCor() << " | ";
             }
             cout << " ]\n\n" << endl;
         }
-
     }
-
 };
 
 #endif // BUSCAEMPROFUNDIDADE_H
